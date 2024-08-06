e956ee8c60fc442e05a93cd7cb61eee7358d2a1b
wangtz <960108176@qq.com>
2024-08-06 16:58:32 +0800

    hotfix

--- api/verifier.go
+++ api/verifier.go
@@ -1,32 +1,10 @@
 package handler
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-
-	emailVerifier "github.com/AfterShip/email-verifier"
-	"github.com/julienschmidt/httprouter"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	verifier := emailVerifier.NewVerifier()
-	ret, err := verifier.Verify(ps.ByName("email"))
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	if !ret.Syntax.Valid {
-		_, _ = fmt.Fprint(w, "email address syntax is invalid")
-		return
-	}
-
-	bytes, err := json.Marshal(ret)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	_, _ = fmt.Fprint(w, string(bytes))
-
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
