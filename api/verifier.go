addace100bc6c2022855457a3701d990761e633d
wangtz <960108176@qq.com>
2024-08-06 17:24:24 +0800

    hotfix

--- api/verifier.go
+++ api/verifier.go
@@ -2,34 +2,33 @@ package Handler
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
-	emailVerifier "github.com/badoux/checkmail"
+	"github.com/badoux/checkmail"
 	"github.com/julienschmidt/httprouter"
 )
 
 // Vercel expects the exported function to be named Handler and match the signature func(http.ResponseWriter, *http.Request)
 func Handler(w http.ResponseWriter, r *http.Request) {
 	// Initialize the email verifier
-	verifier := emailVerifier.NewVerifier()
+	// verifier := emailVerifier.NewVerifier()
 
 	// Extract the email from the request path
 	params := httprouter.ParamsFromContext(r.Context())
 	email := params.ByName("email")
 
 	// Verify the email address
-	ret, err := verifier.Verify(email)
+	ret, err := checkmail.ValidateForma(email)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	// Check if the email syntax is valid
-	if !ret.Syntax.Valid {
-		fmt.Fprint(w, "email address syntax is invalid")
-		return
-	}
+	// if !ret.Syntax.Valid {
+	// 	fmt.Fprint(w, "email address syntax is invalid")
+	// 	return
+	// }
 
 	// Marshal the verification result to JSON
 	bytes, err := json.Marshal(ret)
